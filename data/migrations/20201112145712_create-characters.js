08195201eef35e901526349af6b022242935e352
david hays <purplehays527@gmail.com>
2020-11-12 16:45:47 -0700

    I think that i actually have the set up working now

--- data/migrations/20201112145712_create-characters.js
+++ data/migrations/20201112145712_create-characters.js
@@ -1,8 +1,13 @@
 
 exports.up = function(knex) {
-  
+  return knex.schema.createTable("characters", tbl => {
+      tbl.increments();
+      tbl.string("name",128).notNullable();
+      tbl.string("race",128).notNullable();
+      tbl.string("class",128).notNullable();
+  });
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTableIfExists("characters");
 };
